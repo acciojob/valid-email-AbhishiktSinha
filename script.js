fc8684fb999954854c2adbc9bbe511e892b667a9
AbhishiktSinha <133574794+AbhishiktSinha@users.noreply.github.com>
2023-11-16 01:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function validEmail(str) {
   //your JS code here.
+	if(str.startsWith('@') || str.endsWith('@') || str.indexOf('@')==str.length-2)
+          return false;
+      
+      if(!(str.lastIndexOf('.') > str.indexOf('@')) || (str.lastIndexOf('.')-str.indexOf('@') == 1) || (str.endsWith('.')) || str.startsWith('.'))
+          return false;
+  
+      return true;
 }
 
-// Do not change the code below.
+// // Do not change the code below.
 const str = prompt("Enter an email address.");
 alert(validEmail(str));
